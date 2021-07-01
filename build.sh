ccd83f5923c0851576ebaa83dbcb3ee53431bf83
Victor Olvera <dudeofx@gmx.com>
2021-07-01 14:53:33 -0400

    bug fixes

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-emcc sprite_engine.c tAnimal.c tExplosion.c tGirlWalking.c tGuyWalking.c -s USE_WEBGL2=1 -s ALLOW_MEMORY_GROWTH=1 --preload-file assets --use-preload-plugins -o index.js
+emcc sprite_engine.c tAnimal.c tExplosion.c tGirlWalking.c tGuyWalking.c -s USE_WEBGL2=1 --preload-file assets --use-preload-plugins -o index.js
 
 
